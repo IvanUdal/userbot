e1cc13e230a2eb2777fd4105797e4985bd93cfd8
IvanUdal <75398332+IvanUdal@users.noreply.github.com>
2025-06-21 12:00:46 +0300

    создание

---
+++ config.py
@@ -0,0 +1,14 @@
+# Конфигурация для Telegram Userbot
+# Получите эти данные на https://my.telegram.org
+
+# Ваш API ID (целое число)
+API_ID = 12345678  # Замените на ваш реальный API ID
+
+# Ваш API Hash (строка)
+API_HASH = 'your_api_hash_here'  # Замените на ваш реальный API Hash
+
+# Имя файла сессии
+SESSION_NAME = 'userbot'
+
+# Префикс для команд (по умолчанию точка)
+CMD_PREFIX = '.' 
